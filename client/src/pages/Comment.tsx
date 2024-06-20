b83e473de8b4f1b640157078c4ae29740dc8c8dc
shivansh <shivneeraj2004@gmail.com>
2024-06-20 08:59:36 +0530

    some changes

--- client/src/pages/Comment.tsx
+++ client/src/pages/Comment.tsx
@@ -1,6 +1,7 @@
 import CommentPage from "../components/CommentPage";
 
 export default function Comment() {
+    console.log("hello")
     return (
         <div className="w-full min-h-screen">
             <CommentPage></CommentPage>

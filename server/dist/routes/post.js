3e802916a7858708b04ce90c9adc5f26fdc3d0c1
shivansh <shivneeraj2004@gmail.com>
2024-06-26 05:12:32 +0530

    almost done

--- server/dist/routes/post.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getPost, tweet } from "../controllers/post.js";
-const router = express.Router();
-router.post("/post", tweet);
-router.get("/post", getPost);
-export { router as postRoutes };

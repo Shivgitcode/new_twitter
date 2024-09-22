a718e126b2ac0886b9c9e49b5646a2a83394e9db
shivansh aggarwal <shivneeraj2004@gmail.com>
2024-09-22 22:34:46 +0530

    almost done

--- server/dist/redis/routes/post.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { getPost, likePost, tweet } from "../../controllers/post.js";
-const router = express.Router();
-router.post("/post", tweet);
-router.get("/post", getPost);
-router.post("/likes/:userId/:postId", likePost);
-export { router as postRoutes };
--- server/dist/redis/routes/user.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getUser, login, logout, signup } from "../../controllers/user.js";
-const router = express.Router();
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/logout", logout);
-router.get("/users", getUser);
-export { router };
